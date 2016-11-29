8afd7139195ddf085b5651a0c9145bae5c697299
Myddleware <stephanefaure@crmconsult.fr>
2016-11-30 00:39:12 +0100

    Connecteur shopapplication methodes get_modules, get_module_fields et read_last.
    Signed-off-by: Myddleware <stephanefaure@crmconsult.fr>

--- src/Myddleware/RegleBundle/Solutions/shopapplication.php
+++ src/Myddleware/RegleBundle/Solutions/shopapplication.php
@@ -52,11 +52,12 @@ class shopapplicationcore extends solution {
 	
 	// Connexion to Shop-application
     public function login($paramConnexion) {
+		// Call parent to set $paramConnexion in an attribut of the class 
 		parent::login($paramConnexion);
 		try{	
 			// Delete the "/" at the end of the url if the user have added one
-			$this->url = rtrim($paramConnexion['url'],'/').'/api/';
-			$this->apiKey = '?key='.$paramConnexion['apikey'];
+			$this->url = rtrim($this->paramConnexion['url'],'/').'/api/';
+			$this->apiKey = '?key='.$this->paramConnexion['apikey'];
 			// Try to access to the shop
 			$result = $this->call(trim($this->url.$this->apiKey), 'get', '');	
 			// get the code, if 200 then success otherwise error
@@ -77,7 +78,121 @@ class shopapplicationcore extends solution {
 	} // login($paramConnexion)*/
 
 	 
-	 protected function call($url, $method = 'get', $data=array()){	
+	public function get_modules($type = 'source') {
+		return array(	
+			'customers' => 'Customers',
+			'orders' => 'Order',
+			'products' => 'Products',
+		);
+
+	} // get_modules()	
+	 
+	// Renvoie les champs du module passé en paramètre
+	public function get_module_fields($module, $type = 'source', $extension = false) {
+		parent::get_module_fields($module, $type, $extension);
+		try{
+			// Pour chaque module, traitement différent
+			switch ($module) {
+				case 'customers':
+					$this->moduleFields = array(
+						'id' => array('label' => 'ID customer', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0),
+						'gender' => array('label' => 'Gender', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1, 'option' => array('m' => 'Homme', 'f' => 'Femme')),
+						'first_name' => array('label' => 'First name', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0),
+						'last_name' => array('label' => 'Last_name', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0),
+						'email' => array('label' => 'Email', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'birth_date' => array('label' => 'Birth date', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'phone' => array('label' => 'Phone', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'fax' => array('label' => 'Fax', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'discount' => array('label' => 'Discount', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'credit' => array('label' => 'Credit', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'credit_expiration_date"' => array('label' => 'Credit expiration date', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'reward_points"' => array('label' => 'Reward points', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'password' => array('label' => 'Password', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'date_created' => array('label' => 'Date created', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'auto_connect_url' => array('label' => 'Auto connect url', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 1),
+						'photo' => array('label' => 'Photo', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0)
+					);
+					$this->fieldsRelate = array(
+						'store_id' => array('label' => 'Store ID', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0, 'required_relationship' => 0),
+						'group_id' => array('label' => 'Group ID', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0, 'required_relationship' => 0),
+						'default_address_id' => array('label' => 'Default address id', 'type' => 'varchar(255)', 'type_bdd' => 'varchar(255)', 'required' => 0, 'required_relationship' => 0)
+					);
+					break;
+				default:
+					throw new \Exception("Module ".$module." unknown.");
+					break;
+			}
+			// Ajout des champ relate au mapping des champs 
+			if (!empty($this->fieldsRelate)) {
+				$this->moduleFields = array_merge($this->moduleFields, $this->fieldsRelate);
+			}
+			// Si l'extension est demandée alors on vide relate 
+			if ($extension) {
+				$this->fieldsRelate = array();
+			}
+			return $this->moduleFields;
+		}
+		catch (\Exception $e){
+			$error = $e->getMessage();
+			return false;
+		}
+	} // get_module_fields($module)	 
+	
+	// Read one specific record
+	public function read_last($param) {
+		$result = array();	
+		try {
+// print_r($param);
+			// Simulation : we get the last record
+			if (empty($param['query'])) {
+				$urlApi = $this->url.$param['module'].'/orderby/date_created/desc/limit/1'.$this->apiKey;
+				// http://test-api.shop-application.com/api/customers/orderby/date_created/desc/limit/1?key=jhkCNgPmRUacE4JqMFe4
+				// $urlApi = 'http://test-api.shop-application.com/api/orders/filter/status/equal/25/orderby/date_created/desc/limit/3?key=jhkCNgPmRUacE4JqMFe4';		
+			}
+			// Try to access to the shop
+			$return = $this->call($urlApi, 'get', '');	
+			
+			$code = $return->__get('code');
+			// If the call is a success
+			if ($code == '200') {		
+				$body = $return->__get('body');
+				if (!empty($body)) {
+					// destroy the dimension because we can have only one record
+					$body = current($body);
+// print_r($body);					
+					foreach ($body as $key => $value) {
+						// If the field is requested
+						if(in_array($key, $param['fields'])) {
+// echo $key.' => '.$value.'<BR>';				
+							$result['values'][$key] = $value;
+						}
+					}
+					$result['done'] = true;
+				}
+				else {
+					$result['done'] = false;
+				}
+			}
+			else {
+				// Get the error message
+				$body = $return->__get('body');
+				throw new \Exception('Code error '.$code.(!empty($body->errors->$code) ? ' : '.$body->errors->$code : ''));
+			}			
+		}
+		catch (\Exception $e) {
+		    $result['error'] = 'Error : '.$e->getMessage().' '.__CLASS__.' Line : ( '.$e->getLine().' )';
+			$result['done'] = -1;			
+		}		
+print_r($result);					
+		return $result;
+	}
+	
+	public function read($param) {
+		print_r($param);
+		$result = array();	
+	}
+	
+	protected function call($url, $method = 'get', $data=array()){	
 		if (function_exists('curl_init') && function_exists('curl_setopt')) {
 			$response = \Unirest::$method(
 				$url, // URL de destination
