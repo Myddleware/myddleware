a8f13abbe91a5f68648a7e1799225667f106e101
Myddleware <stephanefaure@crmconsult.fr>
2017-04-12 10:04:12 +0100

    Fix session storing in cache for platform.sh
    Signed-off-by: Myddleware <stephanefaure@crmconsult.fr>

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,6 +3,8 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
+ini_set('session.save_path', __DIR__.'/../app/sessions' );
+
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
