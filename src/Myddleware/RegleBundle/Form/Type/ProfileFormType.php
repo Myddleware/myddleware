524c709bd63b2bd3037eebe825bdb47a0da71b89
jean raynal BEBEL <jeanraynal.bebel@gmail.com>
2018-01-07 23:05:03 +0100

    Add function for : - Change password - Change username and email

---
+++ src/Myddleware/RegleBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Myddleware\RegleBundle\Form\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileFormType extends AbstractType
+{
+
+    /**
+     * delete current_password
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->remove('current_password');
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_profile';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'regle_user_profile';
+    }
+
+}
