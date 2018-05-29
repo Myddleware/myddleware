b4a18ea08b43bda3235123b690354c41c1c613ab
Myddleware <github@myddleware.com>
2018-05-29 14:33:57 +0200

    Release 2.3.4b (#101)
    * Add custom parameters file in composer
    
    * Fix cache issue during upgrade
    
    Signed-off-by: Myddleware <stephanefaure@crmconsult.fr>

--- src/Myddleware/RegleBundle/Classes/upgrade.php
+++ src/Myddleware/RegleBundle/Classes/upgrade.php
@@ -76,7 +76,12 @@ class upgradecore  {
 	public function processUpgrade($output) {
 		try{
 			// Customize update process
-			$this->beforeUpdate($output);	
+			$this->beforeUpdate($output);
+			
+			// Clear every Symfony cache
+			$output->writeln('<comment>Clear Symfony cache...</comment>');
+			$this->clearSymfonycache();
+			$output->writeln('<comment>Clear Symfony cache OK</comment>');			
 			
 		 	// Add new parameters
 			$output->writeln('<comment>Update parameters...</comment>');
@@ -111,12 +116,12 @@ class upgradecore  {
 			$output->writeln('<comment>Update version...</comment>');
 			$this->changeVersion();
 			$output->writeln('<comment>Update version OK</comment>');
-			
-			// Clear every Symfony cache again (otherwise cache folder couldn't be writeable by the web user) 
+
+			// Clear every Symfony cache
 			$output->writeln('<comment>Clear Symfony cache...</comment>');
 			$this->clearSymfonycache();
-			$output->writeln('<comment>Clear Symfony cache OK</comment>');		
-			
+			$output->writeln('<comment>Clear Symfony cache OK</comment>');			
+						
 			// Customize update process
 			$this->afterUpdate($output);
 			
