e52486e0ed269475f884b3bd0cedef6d06046e83
myddleware <stephanefaure@myddleware.com>
2018-10-05 17:20:25 -0400

    Fix bug on data transfer search filter by status
    Signed-off-by: myddleware <stephanefaure@myddleware.com>

--- src/Myddleware/RegleBundle/Controller/FluxController.php
+++ src/Myddleware/RegleBundle/Controller/FluxController.php
@@ -97,7 +97,7 @@ class FluxControllerCore extends Controller
 		//--- Liste status traduction
 		$lstStatusTwig = doc::lstStatus();		
 		foreach ($lstStatusTwig as $key => $value) {
-			$lstStatus[ $key ] = $this->get('translator')->trans( $value );
+			$lstStatus[ $this->get('translator')->trans( $value ) ] = $key;
 		}	
 		asort($lstStatus);
 		//---
@@ -106,7 +106,7 @@ class FluxControllerCore extends Controller
 		$lstGblStatusTwig = doc::lstGblStatus();
 		
 		foreach ($lstGblStatusTwig as $key => $value) {
-			$lstGblStatus[ $key ] = $this->get('translator')->trans( $value );
+			$lstGblStatus[ $this->get('translator')->trans( $value ) ] = $key;
 		}	
 		asort($lstGblStatus);		
 		//---
