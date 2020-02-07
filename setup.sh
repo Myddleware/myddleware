35efbdd1d0236504154b96447d59a35c0b338e79
francesco <info.francescobianco@gmail.com>
2020-02-07 17:31:07 +0100

    publish

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 ## Install dependancies
 docker-compose run --rm myddleware php composer.phar install --ignore-platform-reqs --no-scripts
 
+## Prepare files and directories
+docker-compose run --rm myddleware php composer.phar run-script post-install-cmd
+
 ## Start all containers
 docker-compose up -d
