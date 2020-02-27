8f549838bfb7fc6cc1978295d08326bf765bbdf1
Francesco Bianco <info.francescobianco@gmail.com>
2020-02-27 13:24:41 +0100

    stash changes before fetch upstream

--- fetch-upstream.sh
+++ fetch-upstream.sh
@@ -7,8 +7,10 @@ git commit -am "stash changes before fetch upstream"
 
 git checkout hotfix
 git pull https://github.com/Myddleware/myddleware.git hotfix
+git push
 
 git checkout master
 git pull https://github.com/Myddleware/myddleware.git master
+git push
 
 git checkout stable
