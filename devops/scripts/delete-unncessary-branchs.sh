805827d25e1e4d6d562d143f77ddb74bcdcaafed
Francesco Bianco <info.francescobianco@gmail.com>
2020-06-18 09:28:07 +0200

    fix update script

--- devops/scripts/delete-unncessary-branchs.sh
+++ devops/scripts/delete-unncessary-branchs.sh
@@ -6,6 +6,8 @@ for branch in $( git for-each-ref --format='%(refname:lstrip=3)' refs/remotes/or
     [[ "$branch" == "develop" ]] && git push origin --delete $branch
     [[ "$branch" == "upstream" ]] && git push origin --delete $branch
     [[ "$branch" =~ "analysis-"* ]] && git push origin --delete $branch
+    #[[ "$branch" == "Release-2.5.2" ]] && git push origin --delete $branch
+    #[[ "$branch" == "release-v2.4.2" ]] && git push origin --delete $branch
 done
 
 echo "Done."
