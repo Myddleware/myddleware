b180ec0955927af2d4983cb5ab2652581a6534c4
Francesco Bianco <info.francescobianco@gmail.com>
2020-11-13 11:34:47 +0100

    fix mssql

--- src/Myddleware/RegleBundle/Custom/Solutions/microsoftsql.php
+++ src/Myddleware/RegleBundle/Custom/Solutions/microsoftsql.php
@@ -4,6 +4,15 @@ namespace Myddleware\RegleBundle\Solutions;
 
 class microsoftsqlbase extends microsoftsqlcore
 {
+    public function read($param)
+    {
+        // Redefine reference date format (add milliseconds)
+        $date = new \DateTime($param['date_ref']);
+        $param['date_ref'] = $date->format('Y-m-d H:i:s.v');
+
+        // Call standard read function
+        return parent::read($param);
+    }
 }
 
 $file = __DIR__.'/../Custom/Solutions/microsoftsql.client.php';
