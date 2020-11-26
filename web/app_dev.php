dcaa973d9ee1dcb0912026f1c1e7ce1c2b4f6660
Myddleware <stephanefaure@myddleware.com>
2020-11-26 20:37:29 +0100

    Upgrade to Symfony 4.4
    Signed-off-by: Myddleware <stephanefaure@myddleware.com>

--- web/app_dev.php
+++ web/app_dev.php
@@ -14,7 +14,6 @@ require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
