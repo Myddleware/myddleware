c131cfc955fc5b1ccc191193fcad065ade8152f2
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-02-09 10:35:35 +0100

    WIP

--- src/Myddleware/RegleBundle/Custom/Solutions/vtigercrm.php
+++ src/Myddleware/RegleBundle/Custom/Solutions/vtigercrm.php
@@ -2,22 +2,13 @@
 
 namespace Myddleware\RegleBundle\Solutions;
 
-class microsoftsqlbase extends microsoftsqlcore
+class vtigercrmbase extends vtigercrmcore
 {
-    public function read($param)
-    {
-        // Redefine reference date format (add milliseconds)
-        $date = new \DateTime($param['date_ref']);
-        $param['date_ref'] = $date->format('Y-m-d H:i:s.v');
-
-        // Call standard read function
-        return parent::read($param);
-    }
 }
 
 $file = __DIR__.'/vtigercrm.client.php';
 if (file_exists($file)) {
     require_once $file;
 } else {
-    class microsoftsql extends microsoftsqlbase {}
+    class vtigercrm extends vtigercrmbase {}
 }
--- src/Myddleware/RegleBundle/Custom/Solutions/woocommerce.php
+++ src/Myddleware/RegleBundle/Custom/Solutions/woocommerce.php
@@ -2,22 +2,14 @@
 
 namespace Myddleware\RegleBundle\Solutions;
 
-class microsoftsqlbase extends microsoftsqlcore
+class woocommercebase extends woocommercecore
 {
-    public function read($param)
-    {
-        // Redefine reference date format (add milliseconds)
-        $date = new \DateTime($param['date_ref']);
-        $param['date_ref'] = $date->format('Y-m-d H:i:s.v');
 
-        // Call standard read function
-        return parent::read($param);
-    }
 }
 
 $file = __DIR__.'/woocommerce.client.php';
 if (file_exists($file)) {
     require_once $file;
 } else {
-    class microsoftsql extends microsoftsqlbase {}
+    class woocommerce extends woocommercebase {}
 }
