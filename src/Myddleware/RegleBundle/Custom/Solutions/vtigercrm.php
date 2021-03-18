7cf1aa57ce235287f352003f9abd2895c59c92da
Francesco Bianco <info.francescobianco@gmail.com>
2021-03-18 14:28:09 +0100

    Update vtigercrm.php

--- src/Myddleware/RegleBundle/Custom/Solutions/vtigercrm.php
+++ src/Myddleware/RegleBundle/Custom/Solutions/vtigercrm.php
@@ -4,6 +4,33 @@ namespace Myddleware\RegleBundle\Solutions;
 
 class vtigercrmbase extends vtigercrmcore
 {
+    /**
+     * Inventory modules
+     *
+     * @var string[]
+     */
+    protected $inventoryModules = [
+        'Invoice',
+        'SalesOrder',
+        'Quotes',
+        'PurchaseOrder',
+        'GreenTimeControl',
+        'DDT'
+    ];
+
+    /**
+     * Module list that allows to make parent relationships
+     *
+     * @var string[]
+     */
+    protected $allowParentRelationship = [
+        'Invoice',
+        'Quotes',         
+        'SalesOrder',
+        'PurchaseOrder',
+        'GreenTimeControl',
+        'DDT'
+    ];    
 }
 
 $file = __DIR__.'/vtigercrm.client.php';
