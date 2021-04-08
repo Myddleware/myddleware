7fdf7803cf0a73c254b3870c38f48acfcfd63c75
Francesco Bianco <francescobianco@opencrmitalia.com>
2021-04-08 18:13:35 +0200

    fix importazione

--- src/Myddleware/RegleBundle/Solutions/opencrmitalia.php
+++ src/Myddleware/RegleBundle/Solutions/opencrmitalia.php
@@ -30,6 +30,11 @@ class opencrmitaliacore extends vtigercrm
         'ProductPricebook'
     ];
 
+    /**
+     *
+     */
+    protected $cacheDescribedModules = [];
+
 	/**
 	 * Make the login
 	 *
@@ -118,6 +123,54 @@ class opencrmitaliacore extends vtigercrm
     }
 
     /**
+     * Read Last
+     *
+     * @param array $param
+     * @return array
+     */
+    public function read_last($param)
+    {
+        if (!in_array($param['module'], $this->dbRecordModules)) {
+            return parent::read_last($param);
+        }
+
+        if ($this->notVtigerClient()) {
+            return $this->errorMissingVtigerClient(['done' => -1]);
+        }
+
+        $result = [];
+        $vtigerClient = $this->getVtigerClient();
+
+        if (empty($param['query']['id'])) {
+            throw new \Exception(json_encode($param));
+            $query = $this->getVtigerClient()->query("SELECT {$queryParam} FROM {$module} {$where} ORDER BY modifiedtime DESC LIMIT 0,1;");
+        } elseif (in_array('id', $param['fields'])) {
+            throw new \Exception(json_encode($param));
+            $query = $this->getVtigerClient()->retrieve($param['query']['id']);
+            $query['result'][0] = $query['result'];
+        } else {
+            $query = $this->explodeIdQueryDbRecordModule($param['query']['id'], $param['module']);
+            $read = $vtigerClient->post([
+                'form_params' => [
+                    'operation' => 'dbrecord_crud_row',
+                    'sessionName' => $vtigerClient->getSessionName(),
+                    'name' => $param['module'],
+                    'mode' => 'read',
+                    'element' => json_encode($query),
+                ],
+            ]);
+            if (empty($read['success']) || empty($read['result']['success'])) {
+                throw new \Exception($read["error"]["message"] ?? json_encode($read));
+            }
+            $result['values'] = $read['result']['record'];
+        }
+
+        $result['done'] = true;
+
+        return $result;
+    }
+
+    /**
      * Create new record in target
      *
      * @param array $param
@@ -147,7 +200,7 @@ class opencrmitaliacore extends vtigercrm
                     ],
                 ]);
                 if (empty($create['success']) || empty($create['result']['success'])) {
-                    throw new \Exception($resultCreate["error"]["message"] ?? json_encode($create).' DATA: '.json_encode($data));
+                    throw new \Exception($create["error"]["message"] ?? json_encode($create).' DATA: '.json_encode($data));
                 }
                 $result[$idDoc] = [
                     'id' => $this->assignIdDbRecordModule($param['module'], $create),
@@ -167,10 +220,58 @@ class opencrmitaliacore extends vtigercrm
     }
 
     /**
+     * Update existing record in target
+     *
+     * @param array $param
+     * @return array
+     */
+    public function update($param)
+    {
+        if (!in_array($param['module'], $this->dbRecordModules)) {
+            return parent::update($param);
+        }
+
+        if ($this->notVtigerClient()) {
+            return $this->errorMissingVtigerClient();
+        }
+
+        $result = [];
+        $vtigerClient = $this->getVtigerClient();
+        try {
+            foreach ($param['data'] as $idDoc => $data) {
+                $update = $vtigerClient->post([
+                    'form_params' => [
+                        'operation' => 'dbrecord_crud_row',
+                        'sessionName' => $vtigerClient->getSessionName(),
+                        'name' => $param['module'],
+                        'mode' => 'update',
+                        'element' => json_encode($data),
+                    ],
+                ]);
+                if (empty($update['success']) || empty($update['result']['success'])) {
+                    throw new \Exception($update["error"]["message"] ?? json_encode($update).' DATA: '.json_encode($data));
+                }
+                if (empty($result[$idDoc]['id'])) {
+                    $result[$idDoc]['id'] = $this->assignIdDbRecordModule($param['module'], $update);
+                }
+                $this->updateDocumentStatus($idDoc, $result[$idDoc], $param);
+            }
+        } catch (\Exception $e) {
+            $result['error'] = $e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine();
+        }
+
+        return $result;
+    }
+
+    /**
      *
      */
     protected function describeDbRecordModule($module)
     {
+        if (isset($this->cacheDescribedModules[$module])) {
+            return $this->cacheDescribedModules[$module];
+        }
+
         $vtigerClient = $this->getVtigerClient();
 
         $describe = $vtigerClient->post([
@@ -181,6 +282,12 @@ class opencrmitaliacore extends vtigercrm
                 'mode' => 'describe'
             ],
         ]);
+
+        if (empty($describe['success']) || empty($describe['result']['success'])) {
+            throw new \Exception($describe["error"]["message"] ?? json_encode($describe).' MODULE: '.$module);
+        }
+
+        $this->cacheDescribedModules[$module] = $describe;
 
         return $describe;
     }
@@ -211,6 +318,31 @@ class opencrmitaliacore extends vtigercrm
         #file_put_contents('../var/logs/mio.log', json_encode($create['result'])."\n", FILE_APPEND);
 
         return implode('_', $id);
+    }
+
+    /**
+     * @param $id
+     * @param $module
+     * @return string
+     */
+    protected function explodeIdQueryDbRecordModule($id, $module)
+    {
+        $describe = $this->describeDbRecordModule($module);
+
+        $index = 0;
+        $query = [];
+        $id = explode('_', $id);
+        foreach ($describe['result']['result']['columns'] as $field) {
+            if ($field['isprimarykey']) {
+                //$query[$field['columnname']] = $id[$index];
+                $query[$field['paramname']] = $id[$index];
+                $index++;
+            }
+        }
+
+        #file_put_contents('../var/logs/mio.log', json_encode($create['result'])."\n", FILE_APPEND);
+
+        return $query;
     }
 }
 
