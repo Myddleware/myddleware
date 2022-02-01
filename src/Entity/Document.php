0a12cf4cb7faf4b83c2220809b8d157f90e106c4
Myddleware <stephanefaure@myddleware.com>
2022-02-01 22:36:42 +0100

    Add the possibility to change the user's timezone (+ automatically change the times in the documents and tasks according to the user's timezone (#625)
    Co-authored-by: cpichaud <camillepichaud@myddleware.com>

--- src/Entity/Document.php
+++ src/Entity/Document.php
@@ -26,6 +26,7 @@
 namespace App\Entity;
 
 use DateTime;
+use DateTimezone;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -179,7 +180,7 @@ class Document
     public function __construct()
     {
         $this->datas = new ArrayCollection();
-        $this->logs = new ArrayCollection();
+        $this->logs = new ArrayCollection();   
     }
 
     /**
