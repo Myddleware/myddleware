55b077dd8eeab1ae468904c03066b87230865ece
EstelleMyddleware <estellegaits@myddleware.com>
2022-03-03 16:19:17 +0100

    update regex bootstrap.js

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -4,7 +4,7 @@ import { startStimulusApp } from '@symfony/stimulus-bridge';
 export const app = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
-    /\.[jt]sx?$/
+    /\.(j|t)sx?$/
 ));
 
 // register any custom, 3rd party controllers here
