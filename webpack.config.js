e1a3109b11abc20f5982eeddd27583c95527e9d2
EstelleMyddleware <estellegaits@myddleware.com>
2022-03-10 16:35:36 +0100

    chore/fix: restore gitignore yarn folders

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,11 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+    })
+    
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
