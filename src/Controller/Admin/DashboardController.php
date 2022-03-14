7bf91de088d7e6333fda9ddbe34077c084dabfbf
EstelleMyddleware <estellegaits@myddleware.com>
2022-03-14 16:30:11 +0100

    feat: init EasyAdmin 4 Dashboard - still early stages

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Connector;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -21,12 +22,17 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Myddleware');
+            ->setTitle('<img src="build/images/logo/logo.png" alt="Myddleware">')
+            ->renderContentMaximized();
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return [ MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+                MenuItem::section('Connectors'),
+                MenuItem::linkToCrud('Connector', 'fa fa-link', Connector::class),
+                // MenuItem::linkToCrud('Blog Posts', 'fa fa-file-text', BlogPost::class),
+                // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        ];
     }
 }
