704baa461d44990d4fa43ac4dba6910ade015c7a
EstelleMyddleware <estellegaits@myddleware.com>
2022-04-04 15:56:25 +0200

    feat: add SetupController which will be used to create the install Myddleware wizard

---
+++ src/Controller/SetupController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SetupController extends AbstractController
+{
+    #[Route('/setup', name: 'app_setup')]
+    public function index(): Response
+    {
+        
+        return $this->render('setup/index.html.twig', [
+            'controller_name' => 'SetupController',
+        ]);
+    }
+}
