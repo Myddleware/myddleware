5fe04c328bef68ef8c7c59e102281a0294b0144e
EstelleMyddleware <estellegaits@myddleware.com>
2022-04-11 11:59:40 +0200

    feat: duplicate rule action enabled

--- src/Controller/Admin/RuleCrudController.php
+++ src/Controller/Admin/RuleCrudController.php
@@ -3,23 +3,46 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Rule;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class RuleCrudController extends AbstractCrudController
 {
+    public const ACTION_DUPLICATE = "duplicate";
+
     public static function getEntityFqcn(): string
     {
         return Rule::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+        $duplicate = Action::new(self::ACTION_DUPLICATE)->linkToCrudAction('duplicateRule');
+        return $actions->add(Crud::PAGE_EDIT, $duplicate);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+            TextField::new('nameSlug')->hideOnForm(),
+            CollectionField::new('moduleSource'),
+            CollectionField::new('moduleTarget'),
+            BooleanField::new('active'),
+            BooleanField::new('deleted')->hideOnForm(),
+            DateTimeField::new('createdAt')->hideOnForm(),
+            DateTimeField::new('updatedAt')->hideOnForm()
         ];
     }
-    */
+    
 }
