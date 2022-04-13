ca3fd567525638418720ffe8df69e36515346617
cpichaud <camillepichaud@myddleware.com>
2022-04-13 08:51:15 +0200

    continued, homepage

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/home.css';
 
 // start the Stimulus application
 import './bootstrap';
