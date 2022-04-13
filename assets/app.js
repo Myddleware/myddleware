69631d25b730f5920db3956083a343b5c11d8363
EstelleMyddleware <estellegaits@myddleware.com>
2022-04-13 11:04:47 +0200

    feat: add Bootstrap 5 config

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import './styles/global.scss';
+// You can specify which plugins you need
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
 // start the Stimulus application
 import './bootstrap';
