60338ef870068a58f09d7b1bc0e31e8404ddd57e
EstelleMyddleware <estellegaits@myddleware.com>
2022-06-08 12:08:26 +0200

    feat: solution select : pass the solution to Twig template & interpret HTML as HTML + remove previous content dynamically

--- assets/controllers/solution_controller.js
+++ assets/controllers/solution_controller.js
@@ -1,6 +1,4 @@
 import { Controller } from '@hotwired/stimulus';
-import axios from 'axios';
-const document = window.document;
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
@@ -11,44 +9,16 @@ export default class extends Controller {
         solutionId: Number
     }
 
-    outputElement = null;
-    loginFieldsResponse = null;
-
-    initialize() {
-        this.outputElement = document.createElement('div');
-        this.outputElement.className = 'login-fields';
-        this.outputElement.textContent = 'I will be here';
-        this.element.append(this.outputElement);
-    }
+    htmlOutput = null;
 
     connect() {
-        this.render();
+        this.htmlOutput = document.createElement('div');
     }
 
     async onSelect(event) {
         // // Solution ID 
         this.solutionIdValue = event.currentTarget.value;
         const response = await this.load();
-        // console.log(this.solutionIdValue);
-        // await axios.get(this.infoUrlValue, {
-        //     params: {
-        //         solutionId: this.solutionIdValue
-        //         // page: this.pageValue,
-        //         // offset: this.offsetValue
-        //     }
-        // })
-        //     .then((response) => {
-        //         // this.credentialTarget.innerHTML += response.data;
-        //         // this.element.parentNode.innerHTML += response.data;
-        //         // this.loginFieldsResponse = response.data;
-        //         this.load();
-        //     });
-    }
-
-    render() {
-        const loginFieldsContent = this.credentialTarget.value;
-        // console.log(this.loginFieldsResponse);
-        this.outputElement.innerHTML = this.loginFieldsResponse;
     }
 
     async load() {
@@ -57,7 +27,12 @@ export default class extends Controller {
         })
         const response = await fetch(`${this.infoUrlValue}/${this.solutionIdValue.toString()}`)
             .then(response => response.text())
-            // .then(html => this.element.innerHTML = html)
-            .then(html => this.element.append(html))
+            .then((html) => {
+                this.htmlOutput.innerHTML = html;
+                this.element.append(this.htmlOutput);
+            })
+            .catch(function(err) {  
+                console.log('Failed to fetch response: ', err);  
+            });
     }
 }
--- src/Controller/ConnectorController.php
+++ src/Controller/ConnectorController.php
@@ -9,11 +9,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConnectorController extends AbstractController
 {
-    // #[Route('/credentials/getform/{solutionId<\d+>}', name: 'credentials_form', methods: ['GET'], defaults:['solutionId' => ''])]
     #[Route('/credentials/getform/{solutionId<\d+>?null}', name: 'credentials_form', methods: ['GET'])]
     public function getCredentialsForm(string $solutionId, SolutionRepository $solutionRepository): Response
     {
         $solution = $solutionRepository->find(intval($solutionId));
+        assert(null !== $solution);
+
         return $this->render('connector/index.html.twig', [
             'solution' => $solution
         ]);
