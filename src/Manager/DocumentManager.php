ef49b4457535e691d07c6e55ab7fb8582062c748
myddleware <stephanefaure@myddleware.com>
2022-07-04 02:13:21 +0200

    Add information when the history read is called
    Signed-off-by: myddleware <stephanefaure@myddleware.com>

--- src/Manager/DocumentManager.php
+++ src/Manager/DocumentManager.php
@@ -1153,6 +1153,8 @@ class documentcore
         $read['call_type'] = 'history';
         $read['date_ref'] = '1970-01-01 00:00:00'; // Required field but no needed for history search
         $read['document']['type'] = $this->documentType;
+		$read['document']['id'] = $this->id;
+        $read['jobId'] = $this->jobId;
         $dataTarget = $this->solutionTarget->readData($read);
         // If read method returns no result with no error
 
