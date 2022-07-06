25d31d95ff2ea3d3a49ebb7f8810167605b337e1
Camille Pichaud <camillepichaud@myddleware.com>
2022-07-06 14:35:30 +0200

    feat:if connected, displays button redirects to the administration dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $user = $this->getUser();        
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'user' => $user
         ]);
     }
 }
