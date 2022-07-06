a2fd2ceef662403e2fa6781ea96d9ba4bec47d46
EstelleMyddleware <estellegaits@myddleware.com>
2022-07-06 10:03:13 +0200

    update Rector config & apply run

--- rector.php
+++ rector.php
@@ -2,25 +2,24 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__.'/src',
+return static function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->phpVersion(phpVersion: PhpVersion::PHP_81);
+    $rectorConfig->autoloadPaths(autoloadPaths: [
+        __DIR__ . '/vendor/autoload.php',
     ]);
-
-    // Define what rule sets will be applied
-    $containerConfigurator->import(SetList::DEAD_CODE);
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-
-    // get services (needed for register a single rule)
-    $services = $containerConfigurator->services();
-
-    // register a single rule
-   //  $services->set(TypedPropertyRector::class);
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::PHP_81,
+        SetList::UNWRAP_COMPAT,
+        SetList::TYPE_DECLARATION,
+        SetList::TYPE_DECLARATION_STRICT,
+        SetList::CODING_STYLE,
+    ]);
 };
