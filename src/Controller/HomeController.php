6810f791fae8873a8cc10f387eb281f5c28548b4
Camille Pichaud <camillepichaud@myddleware.com>
2022-07-06 15:47:23 +0200

    fix : homepage-redirect security

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,11 +11,13 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $user = $this->getUser();
-
+        if($user = $this->getUser()){
+            return $this->render('home/index.html.twig', [
+                'user' => $user,
+            ]);
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
             'user' => $user,
-        ]);
+        ]);        
     }
 }
