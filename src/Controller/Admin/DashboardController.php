f16182b826753a0630e16eeb59b2da6f8e1f3e67
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2022-07-06 10:26:14 +0200

    User account (#761)
    * feat: add profile user page
    
    * style: change profile page
    
    * feat: user profile with detail view and edit
    
    * chore:Php-cs-fixer clean up, user profil
    
    Co-authored-by: cpichaud <camillepichaud44@gmail.com>

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,10 +13,12 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -73,5 +75,23 @@ class DashboardController extends AbstractDashboardController
             MenuItem::section('Users'),
             MenuItem::linkToCrud('User', 'fa fa-user', User::class),
         ];
+    }
+
+    /**
+     * @param UserInterface|User $user
+     */
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            // ->setAvatarUrl($user->getAvatarUrl())
+            ->addMenuItems([
+                MenuItem::linkToCrud('My profile', 'fa fa-user', User::class)
+                ->setController(ProfileCrudController::class),
+            ]);
+    }
+
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 }
