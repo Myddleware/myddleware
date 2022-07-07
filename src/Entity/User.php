eb6a31f601c28aabffacb7ad7f89c09bd1140cd4
EstelleMyddleware <estellegaits@myddleware.com>
2022-07-07 15:14:04 +0200

    chore: update packages

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,7 +15,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
-#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
