fa3c77866624c9625c144ff41734df071f5aa4ff
EstelleMyddleware <estellegaits@myddleware.com>
2022-07-07 15:24:00 +0200

    style: Update Rector config to Symfony set lists

--- rector.php
+++ rector.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
+use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->phpVersion(phpVersion: PhpVersion::PHP_81);
@@ -12,13 +14,17 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__.'/vendor/autoload.php',
     ]);
     $rectorConfig->sets([
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::PHP_81,
-        SetList::UNWRAP_COMPAT,
-        SetList::TYPE_DECLARATION,
-        SetList::TYPE_DECLARATION_STRICT,
-        SetList::CODING_STYLE,
+//        SetList::CODE_QUALITY,
+//        SetList::DEAD_CODE,
+//        SetList::EARLY_RETURN,
+//        SetList::PHP_81,
+//        SetList::UNWRAP_COMPAT,
+//        SetList::TYPE_DECLARATION,
+//        SetList::TYPE_DECLARATION_STRICT,
+//        SetList::CODING_STYLE,
+//        SymfonySetList::SYMFONY_52,
+        SymfonyLevelSetList::UP_TO_SYMFONY_54,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ]);
 };
