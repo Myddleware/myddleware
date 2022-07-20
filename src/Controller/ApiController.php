39a4e4d32a308d6060738d30a04b50a74b575835
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2022-07-20 16:31:59 +0200

    Release 3.1.6 (#779)
    * Upgrade myddleware version
    
    * Dev (#778)
    
    * Feat: New cronjob command with a new default timeout (#707)
    
    Signed-off-by: myddleware <stephanefaure@myddleware.com>
    
    * Bugfix/api (#727)
    
    * Bump node-forge from 1.2.1 to 1.3.0 (#695)
    
    Bumps [node-forge](https://github.com/digitalbazaar/forge) from 1.2.1 to 1.3.0.
    - [Release notes](https://github.com/digitalbazaar/forge/releases)
    - [Changelog](https://github.com/digitalbazaar/forge/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/digitalbazaar/forge/compare/v1.2.1...v1.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: node-forge
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Hotfix - Doctrine Exception  (#698)
    
    * hotfix: prevent redirect to login page when no database (triggered by wrong DoctrineException use
    
    * hotfix: update Myddleware Release version
    
    * chore: update packages
    
    * chore: update doctrine extensions bundle recipe
    
    * chore: update webpack-encore-bundle recipe
    
    * fix ApiController call to rule instead of RuleManager
    
    * tests: add AliceBundle for tests fixtures
    
    * tests: API Authentication
    
    * fix: revert to alice bundle old way of writing yaml config file to prevent unrecognised when@dev
    
    * bugfix: API LexikJWT config
    
    * bugfix: API/JWT remove API version from route
    
    * bugfix: API synchro method - retrieve POST params via json_decode
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * delete OpenSSL private & public keys used by LexikJWTAuthenticationBundle
    
    * update MW Release version
    
    * docs: add database setup instructions & remove duplicated docs (#748)
    
    * Template myddleware (#777)
    
    * add an error message, in case of model generation failure
    
    * fix: generate a template
    
    * clean: animation.js
    
    Co-authored-by: Myddleware <stephanefaure@myddleware.com>
    Co-authored-by: EstelleMyddleware <74190794+EstelleMyddleware@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: EstelleMyddleware <estellegaits@myddleware.com>
    
    Co-authored-by: Myddleware <stephanefaure@myddleware.com>
    Co-authored-by: EstelleMyddleware <74190794+EstelleMyddleware@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: EstelleMyddleware <estellegaits@myddleware.com>

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use App\Manager\JobManager;
 use App\Manager\RuleManager;
 use App\Repository\DocumentRepository;
@@ -15,8 +16,13 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 /**
  * @Route("/api", name="api_")
