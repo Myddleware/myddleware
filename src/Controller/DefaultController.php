ccef683bacad765d8385ad9050551b94b70ad0a4
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2022-07-20 16:12:57 +0200

    Template myddleware (#777)
    * add an error message, in case of model generation failure
    
    * fix: generate a template
    
    * clean: animation.js

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2695,7 +2695,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
             try {
                 $choiceSelect = $request->get('choice_select', null);
-                if (null != $choiceSelect) {
+                if (null != $choiceSelect) {                  
                     if ('module' == $choiceSelect) {
                         // si le nom de la règle est inferieur à 3 caractères :
                         if (empty($this->sessionService->getParamRuleSourceSolution($key)) || strlen($this->sessionService->getParamRuleName($key)) < 3) {
@@ -2705,7 +2705,6 @@ use Symfony\Contracts\Translation\TranslatorInterface;
                         }
                         $this->sessionService->setParamRuleSourceModule($key, $request->get('module_source'));
                         $this->sessionService->setParamRuleCibleModule($key, $request->get('module_target'));
-
                         return new Response('module');
                     } elseif ('template' == $choiceSelect) {
                         // Rule creation with the template selected in parameter
@@ -2716,19 +2715,23 @@ use Symfony\Contracts\Translation\TranslatorInterface;
                         $connectorTargetId = (int) $this->sessionService->getParamRuleConnectorCibleId($key);
                         /** @var User $user */
                         $user = $this->getUser();
-
-                        $this->template->convertTemplate($ruleName, $templateName, $connectorSourceId, $connectorTargetId, $user);
+                        try {
+                            $this->template->convertTemplate($ruleName, $templateName, $connectorSourceId, $connectorTargetId, $user);
+                        } catch (Exception $e) {
+                            $this->logger->error($e->getMessage().' '.$e->getFile().' '.$e->getLine());
+                            return  new Response('error');
+                        }
                         // Sort the rules
                         $this->jobManager->orderRules();
                         // We return to the list of rule even in case of error (session messages will be displyed in the UI)/: See animation.js function animConfirm
                         return new Response('template');
                     }
-
+              
                     return new Response(0);
                 }
-
                 return new Response(0);
             } catch (Exception $e) {
+                $this->logger->error($e->getMessage().' '.$e->getFile().' '.$e->getLine());
                 return new Response($e->getMessage());
             }
         }
@@ -2776,7 +2779,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
                 </tbody>
             </table>');
             }
-
+            
             return new Response('<div class="alert alert-warning" role="alert"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16"><path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/><path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/></svg> '.$this->translator->trans('animate.choice.empty').'</div>');
         }
 
