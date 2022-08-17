7b68b85f47d96ec5fb351e53287785d148d8ddd0
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2022-08-17 16:42:56 +0200

    Update ProjectController.php
    feat: database interaction
    wip

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,12 +4,23 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Form\ProjectFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProjectController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     /**
      * @Route("/project", name="app_project")
      */
@@ -21,19 +32,26 @@ class ProjectController extends AbstractController
     /**
      * @Route("/project/create", name="app_create_project")
      */
-    public function create_project(): Response
+    public function create_project(Request $request): Response
     {
 
         $project = new Project();
         $form = $this->createForm(ProjectFormType::class, $project);
-        // $form->handleRequest($request);
+        $form->handleRequest($request);
 
         // dd($form);
 
-        // if ($form->isSubmitted() && $form->isValid()) {
-        // $this->entityManager->flush();
-        // return $this->redirectToRoute('app_project');
-        // }
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $project->setName($form->getData('name'));
+            $project = $form->getData();
+            // $project->setDescription($form->getData('description'));
+            // $project->setName($form->getName());
+            // dd($form->getData());
+            // dd($project);
+            $this->entityManager->persist($project);
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_project');
+        }
 
         return $this->render('project/create.html.twig', [
             'form' => $form->createView()
