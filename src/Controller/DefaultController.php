8a5e4bb265ace7b65315bee70e1e009b3142bb13
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2022-10-31 13:24:42 +0100

    feat: wip add button redirect twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3035,4 +3035,12 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
             return $encrypter->decrypt($tab_params);
         }
+
+        /**
+         * @Route("/rule/executebyid", name="run_by_id")
+         */
+        public function execRuleById()
+        {
+            return $this->render('Rule/byIdForm.html.twig');
+        }
     }
