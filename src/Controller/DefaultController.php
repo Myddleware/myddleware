c9f86eadb2307c7fce5a6bded9c65b8d268dcc2e
myddleware <stephanefaure@myddleware.com>
2023-03-08 13:34:53 +0100

    feat : add parameter to change the number of result of the rule list view
    Signed-off-by: myddleware <stephanefaure@myddleware.com>

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -169,12 +169,13 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
                 $this->getInstanceBdd();
                 $compact['nb'] = 0;
-
+				$pager = $this->tools->getParamValue('ruleListPager');
                 $compact = $this->nav_pagination([
                     'adapter_em_repository' => $this->entityManager->getRepository(Rule::class)->findListRuleByUser($this->getUser()),
-                    'maxPerPage' => isset($this->params['pager']) ? $this->params['pager'] : 20,
+                    'maxPerPage' => isset($pager) ? $pager : 20,
                     'page' => $page,
                 ]);
+
 
                 // Si tout se passe bien dans la pagination
                 if ($compact) {
