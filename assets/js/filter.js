7a44e9aec3efc6db5cac0d4680f0194bfad70dba
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2023-03-13 14:55:12 +0100

    clean: filter.js (#973)
    Co-authored-by: cpichaud <camillepichaud44@gmail.com>

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,66 +1,23 @@
-$(document).ready(function() {
-    // console.log('filter.js loaded');
-    // This function is executed when the document has loaded completely
-
+$(function() {
+    // This function is executed when the value of the 'item_filter_filter' select element changes
     $('#item_filter_filter').on('change', function() {
-        // This function is executed when the value of the 'item_filter_filter' select element changes
 
         var selectedValue = $(this).val();
-        // Get the selected value of the 'item_filter_filter' select element
-
-        $('#combined_filter_document_' + selectedValue).removeAttr('hidden');
-        $('#combined_filter_rule_' + selectedValue).removeAttr('hidden');
-        $('#combined_filter_sourceContent_' + selectedValue).removeAttr('hidden');
-
-        var labelFor = $('label[for="combined_filter_document_' + selectedValue + '"]');
-        var labelForRule = $('label[for="combined_filter_rule_' + selectedValue + '"]');
-        var labelForsourceContent = $('label[for="combined_filter_sourceContent_' + selectedValue + '"]');
-        labelFor.removeAttr('hidden');
-        labelForRule.removeAttr('hidden');
-        labelForsourceContent.removeAttr('hidden');
-        // Show the labels associated with the above elements
-
+        $('#combined_filter_document_' + selectedValue + ', #combined_filter_rule_' + selectedValue + ', #combined_filter_sourceContent_' + selectedValue).removeAttr('hidden');
+        $('label[for="combined_filter_document_' + selectedValue + '"], label[for="combined_filter_rule_' + selectedValue + '"], label[for="combined_filter_sourceContent_' + selectedValue + '"]').removeAttr('hidden');
         $('.' + selectedValue).removeAttr('hidden');
-        // Show all elements with the class name equal to the selected value
 
-
-        // append a input checkbox to the selected value of the select element
-        console.log(selectedValue);
         $('#combined_filter_rule_' + selectedValue).after('<div class="form-check form-switch"><input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" name="inversed_gandalf" value="cabotin"></div>');
-
-
-
     });
 
+    // This function is executed when an element with the 'removeFilter' class is clicked
     $('.removeFilter').on('click', function() {
-        // This function is executed when an element with the 'removeFilter' class is clicked
-
-        var clickedClass = $(this).attr('class');
-        var classes = clickedClass.split(' ');
-        var lastClass = classes[classes.length - 1];
-        // Get the last class name of the clicked element
-
-        $('#combined_filter_document_' + lastClass).attr('hidden', true);
-        // Remove value
-        $('#combined_filter_document_' + lastClass).val('')
-        $('#combined_filter_rule_' + lastClass).attr('hidden', true);
-        $('#combined_filter_rule_' + lastClass).val('');
+        var lastClass = $(this).attr('class').split(' ').pop();
         
-        $('#combined_filter_sourceContent_' + lastClass).attr('hidden', true);
-        $('#combined_filter_sourceContent_' + lastClass).val('');
-
-        // Hide the elements with IDs 'combined_filter_document_{lastClass}' and 'combined_filter_rule_{lastClass}'
-
-        var labelFor = $('label[for="combined_filter_document_' + lastClass + '"]');
-        var labelForRule = $('label[for="combined_filter_rule_' + lastClass + '"]');
-        var labelForsourceContent = $('label[for="combined_filter_sourceContent_' + lastClass + '"]');
-        labelFor.attr('hidden', true);
-        labelForRule.attr('hidden', true);
-        labelForsourceContent.attr('hidden', true);
-        // Hide the labels associated with the above elements
-
+        // Hide all elements with the class name equal to the last class name of the clicked element   
+        $('#combined_filter_document_' + lastClass + ', #combined_filter_rule_' + lastClass + ', #combined_filter_sourceContent_' + lastClass).attr('hidden', true);
+        $('#combined_filter_document_' + lastClass + ', #combined_filter_rule_' + lastClass + ', #combined_filter_sourceContent_' + lastClass).val('');
+        $('label[for="combined_filter_document_' + lastClass + '"], label[for="combined_filter_rule_' + lastClass + '"], label[for="combined_filter_sourceContent_' + lastClass + '"]').attr('hidden', true);
         $('.' + lastClass).attr('hidden', true);
-        // Hide all elements with the class name equal to the last class name of the clicked element      
     });
-
 });
