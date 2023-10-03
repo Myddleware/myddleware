f2d7d37dce865209c0dc3ef98cefeee631eefa15
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2023-10-03 18:23:07 +0200

    Release 3.3.4a (#1073)
    * fix: Timezone isn't used in chart (#1070) -> https://github.com/Myddleware/myddleware/issues/1066
    * fix: date for logs (#1074) -> https://github.com/Myddleware/myddleware/issues/1061

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2635,6 +2635,10 @@ use App\Form\Type\RelationFilterType;
         {
             $countTransferRule = [];
             $jobs = $this->jobRepository->findBy([], ['begin' => 'ASC'], 5);
+            
+            $timeZone = $this->get('session')->get('_timezone', 'UTC'); 
+            $userTimeZone = new \DateTimeZone($timeZone);
+            
             if (count($jobs)) {
                 $countTransferRule[] = [
                     'date',
@@ -2644,8 +2648,11 @@ use App\Form\Type\RelationFilterType;
                     $this->translator->trans('flux.gbl_status.close'),
                 ];
                 foreach ($jobs as $job) {
+                    $start = clone $job->getBegin();
+                    $start->setTimezone($userTimeZone);
+            
                     $countTransferRule[] = [
-                        $job->getBegin()->format('d/m/Y H:i:s'),
+                        $start->format('d/m/Y H:i:s'),
                         (int) $job->getOpen(),
                         (int) $job->getError(),
                         (int) $job->getCancel(),
