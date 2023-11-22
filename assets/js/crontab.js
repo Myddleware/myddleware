5080ae8cacbddcc2db121df9810ff68fabccd2fe
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-11-22 21:14:03 +0100

    feat import future crontabj.s

---
+++ assets/js/crontab.js
@@ -0,0 +1,3 @@
+// Creates a function that will be called when a button with the class crontab_class
+// is clicked. The function will send a request to the server for the function with the following name crontab_show
+console.log('crontab.js');
