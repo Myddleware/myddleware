55685bc007e6e5dcbd01ae7253447cb23ce477cd
myddleware <stephanefaure@myddleware.com>
2024-01-25 08:39:56 +0100

    Airtable new API : add condition to manage 403 error.
    Signed-off-by: myddleware <stephanefaure@myddleware.com>

--- src/Custom/Manager/DocumentManagerCustom.php
+++ src/Custom/Manager/DocumentManagerCustom.php
@@ -481,6 +481,20 @@ class DocumentManagerCustom extends DocumentManager
 			$this->typeError = 'W';
 			$this->message = 'Change status from No_send to Ready_to_send to force the call to Make.';
 		}
+
+		// Manage 403 error with Airtable
+		// It means that the record doesn't exist anymore in Airtable. We can't update it so we create it again.
+		if (
+				!empty($this->document_data)
+			AND $new_status == 'Error_checking'
+			AND	$this->documentType == 'U'
+			AND strpos($this->message, 'HTTP/2 403') !== false
+		) {
+			$new_status = 'Transformed';
+			$this->typeError = 'W';
+			$this->message = 'The record doesn\'t exist in AIrtable anymore. Not possible to update it. We create the record again in Airtable. ';
+			$this->updateType('C');
+		}
 		return $new_status;
 	}
 
