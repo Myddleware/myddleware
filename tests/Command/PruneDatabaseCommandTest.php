913e0969aff71f91e70748babb5aded56ea5abe5
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-05-15 13:25:03 +0200

    working test with fake single document

--- tests/Command/PruneDatabaseCommandTest.php
+++ tests/Command/PruneDatabaseCommandTest.php
@@ -2,28 +2,67 @@
 
 namespace App\Tests\Command;
 
-use App\Command\PruneDatabaseCommand;
-use App\Manager\JobManager;
-use Doctrine\DBAL\Exception;
-use PHPUnit\Framework\TestCase;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class PruneDatabaseCommandTest extends TestCase
+class PruneDatabaseCommandTest extends KernelTestCase
 {
-    private $logger;
-    private $jobManager;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
 
     protected function setUp(): void
     {
-        $this->logger = $this->createMock(LoggerInterface::class);
-        $this->jobManager = $this->createMock(JobManager::class);
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+            $documentId = '66435b3019bad4.47813999';
+
+            // Check if a document with the given ID already exists
+            $sql = "SELECT COUNT(*) FROM `document` WHERE `id` = :id";
+            $count = $this->entityManager->getConnection()->fetchOne($sql, ['id' => $documentId]);
+            
+            if ($count == 0) {
+                // If the document does not exist, insert it
+                $sql = "INSERT INTO `document` VALUES ('66435b3019bad4.47813999', '64807163f1b23', 10, 10, '2024-05-14 12:38:08', '2024-05-14 12:38:08', 'Error_sending', '8', '67', '2023-06-22 06:35:16', '0', 'U', 1, 'Error', '', 1, '')";
+                $this->entityManager->getConnection()->executeStatement($sql);
+
+                // flush the changes to the database
+                $this->entityManager->flush();
+            }
     }
 
     public function testExecute()
     {
-        // do a fake test that 1 + 1 = 2
-        $this->assertEquals(2, 1 + 1);
+        $command = ['php', 'bin/console', 'myddleware:prunedatabase'];
+
+        $process = new Process($command);
+
+        // Run the process
+        try {
+            $process->mustRun();
+
+            // Output the result
+            echo $process->getOutput();
+        } catch (ProcessFailedException $exception) {
+            // Handle the exception
+            echo $exception->getMessage();
+        }
+
+
+        // assert: the document is deleted
+        $documentId = '66435b3019bad4.47813999';
+
+        // Check if a document with the given ID already exists
+        $sql = "SELECT COUNT(*) FROM `document` WHERE `id` = :id";
+        $count = $this->entityManager->getConnection()->fetchOne($sql, ['id' => $documentId]);
+        
+
+        $this->assertEquals(0, $count);
     }
 }
