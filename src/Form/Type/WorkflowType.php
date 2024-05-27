2f9e341f58b56ad81fad8bb5543caf408006212f
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-05-27 10:10:23 +0200

    workflow form

---
+++ src/Form/Type/WorkflowType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form\Type;
+use App\Entity\Rule;
+use App\Repository\RuleRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
+
+
+class WorkflowType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $entityManager = $options['entityManager'];
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Workflow Name',
+                'required' => true,
+            ]);
+            $builder->add('description', TextType::class, ['label' => 'Description']);
+            $builder->add('Rule', EntityType::class, [
+                'class' => Rule::class,
+                'choices' => $options['entityManager']->getRepository(Rule::class)->findBy(['active' => true]),
+                'choice_label' => 'name',
+                'choice_value' => 'id',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ]);
+            $builder->add('workflowCondition', TextareaType::class, ['label' => 'Condition']);
+            $builder->add('submit', SubmitType::class, ['label' => 'Save']);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'App\Entity\Workflow', // Replace with your actual Workflow entity class
+            'entityManager' => null, // Allow the entityManager option
+        ]);
+    }
+}
