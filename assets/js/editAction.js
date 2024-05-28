50eec8d3b3de91963fc3f1088a2456881f221304
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-05-28 14:42:34 +0200

    get action using different id because of dynamic form

--- assets/js/editAction.js
+++ assets/js/editAction.js
@@ -1,14 +1,14 @@
 console.log('test');
         $(document).ready(function() {
             console.log('test2 we are in the function');
-            $('#workflow_action_action').change(function() {
+            $('#form_action').change(function() {
                 console.log('this is the value of the action' + $(this).val());
                 if ($(this).val() === 'sendNotification') {
                     console.log('we show the subject');
-                    $('#workflow_action_subject').parent().show();
+                    $('#form_subject').parent().show();
                 } else {
                     console.log('we hide the subject');
-                    $('#workflow_action_subject').parent().hide();
+                    $('#form_subject').parent().hide();
                 }
             }).trigger('change');
         });
