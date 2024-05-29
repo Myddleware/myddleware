91f4fc0004c008d81d103a3f8f96cd14d6a27301
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-05-29 15:19:01 +0200

    hide the fields and empty the value to avoid saving by mistake, remove else

--- assets/js/editAction.js
+++ assets/js/editAction.js
@@ -10,49 +10,84 @@ console.log('test');
                     $('#form_message').parent().show();
                     // hide searchField
                     $('#form_searchField').parent().hide();
+                    // set the value of the searchField to null
+                    $('#form_searchField').val('');
                     $('#form_searchValue').parent().hide();
+                    // set the value of the searchValue to null
+                    $('#form_searchValue').val('');
                     // hide Rule field
                     $('#form_Rule').parent().hide();
+                    // set the value of the Rule to null
+                    $('#form_Rule').val('');
                     $('#form_status').parent().hide();
-                    // hide form_rerun
+                    //set the value of the status to null
                     $('#form_rerun').parent().hide();
+                    //set the value of the rerun to null
                 } else if ($(this).val() === 'updateStatus') {
                     $('#form_status').parent().show();
                     $('#form_subject').parent().hide();
+                    // set the value of the subject to null
+                    $('#form_subject').val('');
                     $('#form_to').parent().hide();
+                    // set the value of the to to null
+                    $('#form_to').val('');
                     $('#form_message').parent().hide();
+                    // set the value of the message to null
+                    $('#form_message').val('');
                     $('#form_searchField').parent().hide();
+                    // set the value of the searchField to null
+                    $('#form_searchField').val('');
                     $('#form_searchValue').parent().hide();
+                    // set the value of the searchValue to null
+                    $('#form_searchValue').val('');
                     $('#form_Rule').parent().hide();
+                    // set the value of the Rule to null
+                    $('#form_Rule').val('');
                     $('#form_rerun').parent().hide();
+                    // set the value of the rerun to null
+                    $('#form_rerun').val('');
                 } else if ($(this).val() === 'transformDocument') {
                     $('#form_status').parent().hide();
+                    // set the value of the status to null
+                    $('#form_status').val('');
                     $('#form_subject').parent().hide();
+                    // set the value of the subject to null
+                    $('#form_subject').val('');
                     $('#form_to').parent().hide();
+                    // set the value of the to to null
+                    $('#form_to').val('');
                     $('#form_message').parent().hide();
-                    // hide searchField
+                    // set the value of the message to null
+                    $('#form_message').val('');
                     $('#form_searchField').parent().hide();
+                    // set the value of the searchField to null
+                    $('#form_searchField').val('');
                     $('#form_searchValue').parent().hide();
+                    // set the value of the searchValue to null
+                    $('#form_searchValue').val('');
                     $('#form_Rule').parent().hide();
+                    // set the value of the Rule to null
+                    $('#form_Rule').val('');
                     $('#form_rerun').parent().hide();
+                    // set the value of the rerun to null
+                    $('#form_rerun').val('');
                 } else if ($(this).val() === 'generateDocument') {
                     $('#form_subject').parent().hide();
+                    // set the value of the subject to null
+                    $('#form_subject').val('');
                     $('#form_to').parent().hide();
+                    // set the value of the to to null
+                    $('#form_to').val('');
                     $('#form_message').parent().hide();
+                    // set the value of the message to null
+                    $('#form_message').val('');
+                    $('#form_status').parent().hide();
+                    // set the value of the status to null
+                    $('#form_status').val('');
                     $('#form_searchField').parent().show();
                     $('#form_searchValue').parent().show();
                     $('#form_Rule').parent().show();
-                    $('#form_status').parent().hide();
                     $('#form_rerun').parent().show();
-                } else {
-                    $('#form_subject').parent().hide();
-                    $('#form_to').parent().hide();
-                    $('#form_message').parent().hide();
-                    $('#form_searchField').parent().hide();
-                    $('#form_searchValue').parent().hide();
-                    $('#form_Rule').parent().hide();
-                    $('#form_status').parent().hide();
-                    $('#form_rerun').parent().hide();
                 }
 
             }).trigger('change');
