bd81385a3134d3bd46820fd05262825b17003617
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2024-05-29 12:28:33 +0200

    start an elsif for the update status

--- assets/js/editAction.js
+++ assets/js/editAction.js
@@ -8,11 +8,22 @@ console.log('test');
                     $('#form_subject').parent().show();
                     $('#form_to').parent().show();
                     $('#form_message').parent().show();
+                    // hide searchField
+                    $('#form_searchField').parent().hide();
+                    $('#form_searchValue').parent().hide();
+                } else if ($(this).val() === 'updateStatus') {
+                    $('#form_subject').parent().hide();
+                    $('#form_to').parent().hide();
+                    $('#form_message').parent().hide();
+                    // hide searchField
+                    $('#form_searchField').parent().hide();
+                    $('#form_searchValue').parent().hide();
                 } else {
                     console.log('we hide the subject');
                     $('#form_subject').parent().hide();
                     $('#form_to').parent().hide();
                     $('#form_message').parent().hide();
                 }
+
             }).trigger('change');
         });
