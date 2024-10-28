437ccbc4aadc63687c72699dd349c75c26bb0a7d
AlexMyddleware <alexistroitzky@myddleware.com>
2024-10-28 09:04:07 +0100

    lift character limit for search

--- assets/js/rulelist.js
+++ assets/js/rulelist.js
@@ -22,21 +22,18 @@ $("#rule_name").on("keyup input", function() {
     var ruleName = $(this).val();
     var url = $(this).data('url');
 
-    if (ruleName.length >= 3) {
+    if (ruleName.length > 0) {
         $.ajax({
             type: "GET",
             url: url,
             data: { rule_name: ruleName },
             success: function(data){
-                // parse the returned data and load it into a jQuery object
                 var html = $(data);
-                // find the tbody element
                 var tbody_html = html.find('#tbody_rule_list').html();
-                // replace the tbody in the current page with the tbody from the returned data
                 $('#tbody_rule_list').html(tbody_html);
             }
         });
-    } else if (ruleName.length == 0) {
+    } else {
         // if the search bar is empty, reset the table
         $('#tbody_rule_list').html(initialTableState);
     }
