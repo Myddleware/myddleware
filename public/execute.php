103f8971d1d04d834dbfe6ed56136945b8574b4d
AlexMyddleware <alexistroitzky@myddleware.com>
2024-11-27 14:56:33 +0100

    removing // execute.php because it messes with the response

--- public/execute.php
+++ public/execute.php
@@ -1,8 +1,8 @@
-// execute.php
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json'); // Ensure the response is JSON
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $input = json_decode(file_get_contents('php://input'), true);
@@ -11,10 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Execute the command and capture the output
     $output = shell_exec($command);
 
-    error_log("output:");
-    error_log($output);
-
-    // Return the output
+    // Return the output as JSON
     echo json_encode(['output' => $output]);
 }
 ?>
