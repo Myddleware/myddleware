6dc19078ae614fdb14268ca2aa027f06356a6892
AlexMyddleware <alexistroitzky@myddleware.com>
2024-11-27 14:42:07 +0100

    use hardcoded path for now, logging statement, handling cors

--- public/execute.php
+++ public/execute.php
@@ -1,13 +1,20 @@
 // execute.php
-    <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $input = json_decode(file_get_contents('php://input'), true);
-        $command = escapeshellcmd($input['command']); // Sanitize input
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
-        // Execute the command and capture the output
-        $output = shell_exec($command);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $input = json_decode(file_get_contents('php://input'), true);
+    $command = escapeshellcmd($input['command']); // Sanitize input
 
-        // Return the output
-        echo json_encode(['output' => $output]);
-    }
-    ?>
+    // Execute the command and capture the output
+    $output = shell_exec($command);
+
+    error_log("output:");
+    error_log($output);
+
+    // Return the output
+    echo json_encode(['output' => $output]);
+}
+?>
