5a40d260092fa9b0a8cbe5f2fbfa8bea23ec6cfe
AlexMyddleware <alexistroitzky@myddleware.com>
2024-11-29 14:36:32 +0100

    update deploy to get 5 seconds between each step

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,28 @@
 
 echo " [1/6]===> Pulling the latest changes from the release-3.4.0a branch"
 git pull origin release-3.4.0a
+sleep 5
 
 echo " [2/6]===> Installing the dependencies"
 composer install
+sleep 5
 
 echo " [3/6]===> Installing the node dependencies"
 nvm install 16
-
 yarn install
+sleep 5
 
 echo " [4/6]===> Building the assets"
 yarn build
+sleep 5
 
 echo " [5/6]===> Updating the database schema"
 php bin/console doctrine:schema:update --force
+sleep 5
 
 echo " [6/6]===> Clearing the cache"
 rm -rf var/cache/background/*
-
 rm -rf var/cache/dev/*
+sleep 5
 
 echo "===> Deployment completed successfully"
