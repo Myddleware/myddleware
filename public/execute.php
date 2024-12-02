e72ece78985d4e5e7b3f878fdcd124f557cedcc2
AlexMyddleware <alexistroitzky@myddleware.com>
2024-12-02 14:49:16 +0100

    clean code

--- public/execute.php
+++ public/execute.php
@@ -10,63 +10,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Check if shell_exec function is disabled
     if (!function_exists('shell_exec') || in_array('shell_exec', array_map('trim', explode(',', ini_get('disable_functions'))))) {
-        // throw new \Exception('The PHP shell_exec() function is disabled. Please enable it in php.ini to run the terminal.');
         echo json_encode(['output' => 'The PHP shell_exec() function is disabled. Please enable it in php.ini to run the terminal.']);
         exit;
     }
 
+    $phpPath = trim(shell_exec(isWindows() ? 'where php' : 'which php'));
+    $executepath = $_SERVER['SCRIPT_FILENAME'];
+    $cleanPath = str_replace(isWindows() ? '\\public\\execute.php' : '/public/execute.php', '', $executepath);
+    $consolePath = $cleanPath . (isWindows() ? '\\bin\\console' : '/bin/console');
 
-
-   $phpPath = trim(shell_exec('where php'));
-
-   if (isWindows()) {
-    $phpPath = adjustPath($phpPath);
-   }
-
-   $executepath = $_SERVER['SCRIPT_FILENAME'];
-
-   if (isWindows()) {
-    $executepath = adjustPath($executepath);
-   }
-   
-   if (isWindows()) {
-    $cleanPath = str_replace('\\public\\execute.php', '', $executepath);
-   } else {
-    $cleanPath = str_replace('/public/execute.php', '', $executepath);
-   }
-
-   if (isWindows()) {
-    $cleanPath = adjustPath($cleanPath);
-   }
-
-   if (isWindows()) {
-    $consolePath = $cleanPath . '\bin\console';
-   } else {
-    $consolePath = $cleanPath . '/bin/console';
-   }
-
-   if (isWindows()) {
-    $consolePath = adjustPath($consolePath);
-   }
-
-//    if the command contains php bin/console
-if (strpos($command, 'php bin/console') !== false) {
-    $isolatedCommand = str_replace('php bin/console', '', $command);
-    $output = shell_exec("$phpPath $consolePath $isolatedCommand 2>&1");
-} elseif (strpos($command, 'composer') !== false) {
-    $isolatedCommand = str_replace('composer ', '', $command);
-
-    $composerPath = trim(shell_exec('which composer'));
-
-
+    // Adjust paths for Windows
     if (isWindows()) {
-        $composerPath = adjustPath($composerPath);
+        $phpPath = adjustPath($phpPath);
+        $executepath = adjustPath($executepath);
+        $cleanPath = adjustPath($cleanPath);
+        $consolePath = adjustPath($consolePath);
     }
 
-    $output = shell_exec("cd $cleanPath && $composerPath $isolatedCommand 2>&1");
-} else {
-    $output = shell_exec($command);
-}
+    // Execute command
+    if (strpos($command, 'php bin/console') !== false) {
+        $isolatedCommand = str_replace('php bin/console', '', $command);
+        $output = shell_exec("$phpPath $consolePath $isolatedCommand 2>&1");
+    } elseif (strpos($command, 'composer') !== false) {
+        $isolatedCommand = str_replace('composer ', '', $command);
+        $composerPath = trim(shell_exec(isWindows() ? 'where composer' : 'which composer'));
+
+        if (isWindows()) {
+            $composerPath = adjustPath($composerPath);
+        }
+
+        $output = shell_exec("cd $cleanPath && $composerPath $isolatedCommand 2>&1");
+    } else {
+        $output = shell_exec($command);
+    }
 
     // Return the output as JSON
     echo json_encode(['output' => $output]);
