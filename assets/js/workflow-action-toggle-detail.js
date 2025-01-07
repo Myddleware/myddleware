68c336bd19d0c392f62642484a9de26d287ccfc6
AlexMyddleware <alexistroitzky@myddleware.com>
2025-01-07 20:42:38 +0100

    make code more concise https://myddleware.atlassian.net/browse/PROJ-383

--- assets/js/workflow-action-toggle-detail.js
+++ assets/js/workflow-action-toggle-detail.js
@@ -1,26 +1,18 @@
-var path = window.location.href;
+const workflowActionId = window.location.href
+    .replace('showAction', 'active')
+    .split('/')
+    .pop();
 
-// replace showAction by active
-path = path.replace('showAction', 'active');
+const apiUrl = window.location.href
+    .replace('showAction', 'active')
+    .replace(new RegExp(`${workflowActionId}/?$`), '');
 
-const workflowActionId = path.split('/').pop();
-
-// remove the id from the path
-path = path.replace(workflowActionId, '');
-
-// remove final slash
-path = path.replace(/\/$/, '');
-
-// Get the checkbox input using the class and data attributes
 document.addEventListener('DOMContentLoaded', function() {
-    // get the button using the id of the workflow action
-    const toggleButton = document.getElementById('activeWorkflow_'+workflowActionId);
+    const toggleButton = document.getElementById('activeWorkflow_' + workflowActionId);
     
     if (toggleButton) {
-        // Add event listener if needed
         toggleButton.addEventListener('change', function(event) {
-            // we send a request to the server to update the status of the workflow action
-            fetch(`${path}/${workflowActionId}`, {
+            fetch(`${apiUrl}${workflowActionId}`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -28,19 +20,13 @@ document.addEventListener('DOMContentLoaded', function() {
                 body: JSON.stringify({ active: event.target.checked }),
             })
             .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
+                if (!response.ok) throw new Error('Network response was not ok');
                 return response.json();
             })
             .then(data => {
-                if (!data.success) {
-                    // Handle error
-                    throw new Error(data.message);
-                }
+                if (!data.success) throw new Error(data.message);
             })
-            .catch(error => {
-                // Maybe revert the checkbox state
+            .catch(() => {
                 event.target.checked = !event.target.checked;
             });
         });
