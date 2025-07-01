cdde22c5a675a1c858a22707e9d96b16ee0cdcad
cpichaud <camillepichaud44@gmail.com>
2025-07-01 09:26:57 +0200

    add create user

--- src/Controller/UserManagerController.php
+++ src/Controller/UserManagerController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,7 +12,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TimezoneType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserManagerController extends AbstractController
 {
@@ -57,6 +59,52 @@ class UserManagerController extends AbstractController
         return $this->render('UserManager/edit.html.twig', [
             'form' => $form->createView(),
             'user' => $user,
+        ]);
+    }
+
+    #[Route('/rule/user_manager/new', name: 'user_manager_create')]
+    public function create(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                    'Super Admin' => 'ROLE_SUPER_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => false,
+            ])
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'required' => true,
+                'label' => 'Mot de passe'
+            ])
+            ->add('timezone', TimezoneType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create User'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user->setUsernameCanonical(strtolower($user->getUsername()));
+            $user->setEmailCanonical(strtolower($user->getEmail()));
+            $hashedPassword = $passwordHasher->hashPassword($user, $form->get('password')->getData());
+            $user->setPassword($hashedPassword);
+
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'success');
+            return $this->redirectToRoute('user_manager');
+        }
+
+        return $this->render('UserManager/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
