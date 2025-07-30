86b14b307947fa381aa4e5d6b3dcec1d7d6b138c
cpichaud <camillepichaud44@gmail.com>
2025-07-30 11:46:13 +0200

    update mapping field, rule detail

--- assets/js/mappingRule.js
+++ assets/js/mappingRule.js
@@ -1,26 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to toggle the comment box visibility
-    window.toggleCommentBox = function(button) {
-        const commentBox = button.nextElementSibling;
-        commentBox.style.display = commentBox.style.display === "block" ? "none" : "block";
+    // open/close comment-box
+    window.toggleCommentBox = function (button) {
+        const cardBody = button.closest('.mapping-card-body');
+        const commentBox = cardBody.querySelector('.comment-box');
+
+        if (commentBox) {
+            commentBox.style.display =
+                commentBox.style.display === 'block' ? 'none' : 'block';
+        }
     };
 
-    // Function to hide the comment box
-    window.closeCommentBox = function(icon) {
-        icon.parentElement.style.display = "none";
+    // close comment-box
+    window.closeCommentBox = function (icon) {
+        icon.closest('.comment-box').style.display = 'none';
     };
-
-    document.querySelectorAll('.comment-box form').forEach(form => {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Prevent default form submission
+    
+    document.querySelectorAll('.comment-box form').forEach((form) => {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
 
             const formData = new FormData(form);
             const actionUrl = form.getAttribute('action');
-            const commentText = form.querySelector('.comment-textarea').value.trim();
+            const commentText = form
+                .querySelector('.comment-textarea')
+                .value.trim();
             let commentOutside = form.closest('.comment-box').nextElementSibling;
 
-            if (commentText === "") {
-                alert("Le commentaire ne peut pas être vide."); // Alert if comment is empty
+            if (commentText === '') {
+                alert('Le commentaire ne peut pas être vide.');
                 return;
             }
 
@@ -28,25 +35,29 @@ document.addEventListener('DOMContentLoaded', function () {
                 method: 'POST',
                 body: formData,
             })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erreur lors de la mise à jour du commentaire');
-                }
-                return response.text();
-            })
-            .then(data => {
-                if (!commentOutside) {
-                    commentOutside = document.createElement('div');
-                    commentOutside.className = 'comment-outside';
-                    form.closest('.comment-box').after(commentOutside);
-                }
-                commentOutside.textContent = commentText;
-                form.closest('.comment-box').style.display = 'none';
-            })
-            .catch(error => {
-                console.error('Erreur:', error);
-                alert("Une erreur s'est produite lors de la mise à jour du commentaire.");
-            });
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(
+                            'Erreur lors de la mise à jour du commentaire'
+                        );
+                    }
+                    return response.text();
+                })
+                .then(() => {
+                    if (!commentOutside) {
+                        commentOutside = document.createElement('div');
+                        commentOutside.className = 'comment-outside';
+                        form.closest('.comment-box').after(commentOutside);
+                    }
+                    commentOutside.textContent = commentText;
+                    form.closest('.comment-box').style.display = 'none';
+                })
+                .catch((error) => {
+                    console.error('Erreur:', error);
+                    alert(
+                        "Une erreur s'est produite lors de la mise à jour du commentaire."
+                    );
+                });
         });
     });
 });
