9797af8762d7e8fb05d43305a893da8d27f468a9
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2025-08-07 10:31:53 +0200

    Fix update (#1319)
    * fix document home page
    
    * fix home page
    
    * fix rule group
    
    * remove front mapping and update rule detail
    
    * add icon for mapping
    
    * add button +/- for formula
    
    * add search field in mapping
    
    ---------
    
    Co-authored-by: cpichaud <camillepichaud44@gmail.com>

--- assets/js/mappingRule.js
+++ assets/js/mappingRule.js
@@ -1,33 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // open/close comment-box
     window.toggleCommentBox = function (button) {
-        const cardBody = button.closest('.mapping-card-body');
-        const commentBox = cardBody.querySelector('.comment-box');
-
-        if (commentBox) {
-            commentBox.style.display =
-                commentBox.style.display === 'block' ? 'none' : 'block';
-        }
+        const commentBox = button.nextElementSibling;
+        commentBox.style.display = commentBox.style.display === "block" ? "none" : "block";
     };
 
-    // close comment-box
     window.closeCommentBox = function (icon) {
-        icon.closest('.comment-box').style.display = 'none';
+        icon.parentElement.style.display = "none";
     };
-    
-    document.querySelectorAll('.comment-box form').forEach((form) => {
+
+    document.querySelectorAll('.comment-box form').forEach(form => {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
 
             const formData = new FormData(form);
             const actionUrl = form.getAttribute('action');
-            const commentText = form
-                .querySelector('.comment-textarea')
-                .value.trim();
+            const commentText = form.querySelector('.comment-textarea').value.trim();
             let commentOutside = form.closest('.comment-box').nextElementSibling;
 
-            if (commentText === '') {
-                alert('Le commentaire ne peut pas être vide.');
+            if (commentText === "") {
+                alert("Comment cannot be empty.");
                 return;
             }
 
@@ -35,15 +26,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 method: 'POST',
                 body: formData,
             })
-                .then((response) => {
+                .then(response => {
                     if (!response.ok) {
-                        throw new Error(
-                            'Erreur lors de la mise à jour du commentaire'
-                        );
+                        throw new Error('Error updating comment');
                     }
                     return response.text();
                 })
-                .then(() => {
+                .then(data => {
                     if (!commentOutside) {
                         commentOutside = document.createElement('div');
                         commentOutside.className = 'comment-outside';
@@ -52,12 +41,77 @@ document.addEventListener('DOMContentLoaded', function () {
                     commentOutside.textContent = commentText;
                     form.closest('.comment-box').style.display = 'none';
                 })
-                .catch((error) => {
+                .catch(error => {
                     console.error('Erreur:', error);
-                    alert(
-                        "Une erreur s'est produite lors de la mise à jour du commentaire."
-                    );
+                    alert("An error occurred while updating the comment.");
                 });
+        });
+    });
+
+    const toggle = document.getElementById('info-toggle');
+    let isFixed = false;
+    let hoverTimeout;
+
+    function expand() {
+        toggle.classList.add('expanded');
+    }
+
+    function collapse() {
+        if (!isFixed) toggle.classList.remove('expanded');
+    }
+
+    toggle.addEventListener('mouseenter', () => {
+        clearTimeout(hoverTimeout);
+        expand();
+    });
+
+    toggle.addEventListener('mouseleave', () => {
+        if (!isFixed) {
+        hoverTimeout = setTimeout(collapse, 200);
+        }
+    });
+
+    toggle.addEventListener('click', () => {
+        isFixed = !isFixed;
+        if (isFixed) {
+        expand();
+        } else {
+        collapse();
+        }
+    });
+
+    window.toggleFormula = function (button) {
+        const preview = button.parentElement.querySelector('.formula-preview');
+        const full = button.parentElement.querySelector('.formula-full');
+
+        const isExpanded = !full.classList.contains('d-none');
+
+        if (isExpanded) {
+            full.classList.add('d-none');
+            preview.classList.remove('d-none');
+            button.innerHTML = '<i class="fa fa-plus" aria-hidden="true"></i>';
+        } else {
+            full.classList.remove('d-none');
+            preview.classList.add('d-none');
+            button.innerHTML = '<i class="fa fa-minus"></i>';
+        }
+    };
+
+    const searchInput = document.getElementById('field-filter');
+    const cards = document.querySelectorAll('.mapping-card');
+
+    searchInput.addEventListener('input', function () {
+        const query = this.value.trim().toLowerCase();
+
+        cards.forEach(card => {
+            const target = card.getAttribute('data-target');
+            const source = card.getAttribute('data-source');
+
+            if (target.includes(query) || source.includes(query)) {
+                card.style.display = '';
+            } else {
+                card.style.display = 'none';
+            }
         });
     });
 });
