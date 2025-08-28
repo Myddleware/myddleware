4e8bbd02af2be606e7aeb92347c41e5f0b61b18c
AlexMyddleware <alexistroitzky@myddleware.com>
2025-08-28 09:21:58 +0200

    remove Exception listener

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -20,13 +20,5 @@ class ExceptionListener
     public function onKernelException(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
-
-        // we intercept exceptions to do with the fact that user hasn't set up his database parameters yet
-        if ($exception instanceof ConnectionException || $exception instanceof TableNotFoundException) {
-            $urlInstall = $this->router->generate('install_requirements');
-            // redirect to installation page
-            $response = new RedirectResponse($urlInstall);
-            $event->setResponse($response);
-        }
     }
 }
