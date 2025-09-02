a8d2f06d6f7db23ccb644955fca2120e31c5b9c3
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2025-09-02 11:18:04 +0200

    Fix dev front (#1330)
    * fix title, detail variable
    
    * fix field in install
    
    * fix, version sass and rule detail
    
    * fix, rule detail, params and filter
    
    * fix, rule group
    
    ---------
    
    Co-authored-by: cpichaud <camillepichaud44@gmail.com>

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -18,9 +19,23 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('email', EmailType::class)
+            ->add('username', TextType::class, [ 
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Username',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Email',
+                ],
+            ])
             ->add('plainPassword', PasswordType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Password',
+                ],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
