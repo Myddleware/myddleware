d2cf5311d1f86cfc67f4855c12106d1a0c176afd
AlexMyddleware <alexistroitzky@myddleware.com>
2025-10-02 10:53:10 +0200

    test with updated path

--- assets/js/connector.js
+++ assets/js/connector.js
@@ -140,9 +140,14 @@ $( function() {
 								$('#msg_status').show();
 							}
 							else{
+								let pathString = "../";
+                                // if the window path contains index.php (docker)
+                                if (window.location.pathname.includes("index.php")) {
+                                    pathString = "../../../../";
+                                }	
 								status.removeAttr("src");
                                         console.log('004 myddleware status online')
-								status.attr("src","../" +path_img+"status_online.png");
+								status.attr("src", pathString + path_img + "status_online.png");
 								$('#msg_status').hide();
 								$('#msg_status span.error').html('');
 							}						
