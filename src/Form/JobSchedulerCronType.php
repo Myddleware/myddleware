cced8f7c036b4004b690bed59155d038b1a64852
Camille Pichaud <95077335+CamillePMyddleware@users.noreply.github.com>
2025-10-08 17:20:41 +0200

    fix: creation of a new crontab (#1360)
    Co-authored-by: cpichaud <camillepichaud44@gmail.com>

--- src/Form/JobSchedulerCronType.php
+++ src/Form/JobSchedulerCronType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -23,7 +25,19 @@ class JobSchedulerCronType extends AbstractType
             ->add('description', TextType::class)
             ->add('runningInstances', IntegerType::class, ['mapped' => false])
             ->add('maxInstances', IntegerType::class)
-            ->add('period', TextType::class)
+             ->add('period', TextType::class, [
+                'constraints' => [
+                    new Assert\Regex([
+                        'pattern' => '/^\s*\S+\s+\S+\s+\S+\s+\S+\s+\S+\s*$/',
+                        'message' => 'Invalid cron expression. Example: */5 * * * *',
+                    ]),
+                ], 'attr' => [
+                    'required' => true,
+                    'placeholder' => '*/5 * * * *',
+                    'pattern' => '\S+\s+\S+\s+\S+\s+\S+\s+\S+',
+                    'title' => '5 fields separated by spaces (*/5 * * * *)',
+                ],
+            ])
             ->add('save', SubmitType::class, [
             'attr' => [
                 'class' => 'btn btn-success',
