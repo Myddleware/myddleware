3dd52b0cbbb2b10222ee4a84a7f9f0267d619f6d
Stephane Faure <stephanefaure@myddleware.com>
2025-12-26 23:14:32 +0100

    Hotfix : change the global status of status Error_expected from Error to Cancel
    Signed-off-by: Stephane Faure <stephanefaure@myddleware.com>

--- src/Manager/DocumentManager.php
+++ src/Manager/DocumentManager.php
@@ -111,7 +111,7 @@ class DocumentManager
         'Error_transformed' => 'Error',
         'Error_checking' => 'Error',
         'Error_sending' => 'Error',
-        'Error_expected' => 'Error',
+        'Error_expected' => 'Cancel',
         'Not_found' => 'Error',
     ];
     private array $notSentFields = [];
